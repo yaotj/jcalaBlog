44294c356b8ba902b46940ebd925da7b08cf1af1
jcala <jcalaz@163.com>
2016-09-27 21:06:53 +0800

    设置程序第一次运行时初始化数据库结构和数据

--- src/main/java/me/jcala/blog/Application.java
+++ src/main/java/me/jcala/blog/Application.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableCaching
+@SpringBootApplication
+@EnableTransactionManagement//开启事务
+@EnableCaching//开启ehcache缓存
 public class Application extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
